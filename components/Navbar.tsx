2c0417eddec7c7ef1d94a3417770ba17dcea9ed0
Joy Lodra <joylodra@gmail.com>
2022-11-02 16:18:03 +0700

    Changed logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
+import { FaVideo } from 'react-icons/fa';
 
 import useAuthStore from '../store/authStore';
 
@@ -29,12 +30,10 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="w-[100px] md:w-[130px]">
-          <Image
-            className="cursor-pointer"
-            src={Logo}
-            alt="TikTik"
-          />
+        <div className="w-[100px] md:w-[130px] font-extrabold text-transparent 
+        text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 flex
+        items-center gap-1">
+          <FaVideo className="text-pink-600" /> JOYVIDS
         </div>
       </Link>
 
